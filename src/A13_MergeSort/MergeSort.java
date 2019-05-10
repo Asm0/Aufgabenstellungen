33e6a2a44f0fba1f78241e2f1f958fc3cf2f8917
Andreas Smolle <smollea@gmail.com>
2019-05-10 21:07:05 +0200

    A13_MergeSort fertiggestellt

--- src/A13_MergeSort/MergeSort.java
+++ src/A13_MergeSort/MergeSort.java
@@ -21,14 +21,19 @@ public class MergeSort implements PersonenSort {
 	 */
 	public void sort(Person[] personen, int start, int end)
 	{
-		// TODO: Aufteilung & Rekursion implementieren
-		
-		int mitte = 0;
+		int mitte = end/2;
 		
 		// Für Merge: Hälften in eigene Arrays kopieren
 		// Hinweis: bei copyOfRange ist Obergrenze exklusiv, deshalb "+ 1"
-		Person[] teil1 = Arrays.copyOfRange(personen, start, mitte+1);
-		Person[] teil2 = Arrays.copyOfRange(personen, mitte+1, end+1);
+		if(personen.length <= 1) {
+			return;		// Wenn Personen Array nur mehr einen Wert hat, brich Rekursion ab
+		}
+			Person[] teil1 = Arrays.copyOfRange(personen, start, mitte+1);
+			Person[] teil2 = Arrays.copyOfRange(personen, mitte+1, end+1);
+
+			sort(teil1, 0, teil1.length-1);
+			sort(teil2, 0, teil2.length-1);
+
 		// Beide Hälften zusammenfügen und in data-Array schreiben
 		merge(teil1, teil2, personen, start);
 	}
@@ -41,9 +46,29 @@ public class MergeSort implements PersonenSort {
 	 * @param start  Position für Speicherung in Ergebnisarray
 	 */
 	public void merge(Person[] pers1, Person[] pers2, Person[] result, int start) {
-
-		// TODO: Merge implementieren
-		
+		int leftInd = 0;
+		int rightInd = 0;
+		int resultInd = 0;
+		while (leftInd < pers1.length && rightInd < pers2.length) {
+			if(pers1[leftInd].compareTo(pers2[rightInd]) <= 0){
+				result[resultInd] = pers1[leftInd];
+				leftInd++;
+			} else {
+				result[resultInd] = pers2[rightInd];
+				rightInd++;
+			}
+			resultInd++;
+		}
+		while (leftInd < pers1.length) {
+			result[resultInd] = pers1[leftInd];
+			leftInd++;
+			resultInd++;
+		}
+		while (rightInd < pers2.length) {
+			result[resultInd] = pers2[rightInd];
+			rightInd++;
+			resultInd++;
+		}
 	}
 
 }
