9407439881c6010c87e33d7cb7e7fb51309b0373
Andreas Smolle <smollea@gmail.com>
2019-05-27 12:35:23 +0200

    A07_BubbleSort implementiert

--- src/A07_BubbleSort/BubbleSort.java
+++ src/A07_BubbleSort/BubbleSort.java
@@ -7,7 +7,21 @@ public class BubbleSort implements PersonenSort {
 	 * Sortier-Funktion
 	 */
 	public void sort(Person[] personen) {
-
+		Boolean sorted = false;
+		int sorted_numbers = 0;
+		while (!sorted) {
+			sorted = true;
+			for (int i = 0; i < personen.length-1-sorted_numbers; i++) {
+				Person p = personen[i];
+				Person p2 = personen[i+1];
+				if(p.compareTo(p2) > 0){
+					sorted = false;
+					personen[i] = p2;
+					personen[i+1] = p;
+				}
+			}
+			sorted_numbers++;
+		}
 	}
 	
 }
