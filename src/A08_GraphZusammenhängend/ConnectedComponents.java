73e61336dd44eeae10be8ce0eea379223d94671e
Andreas Smolle <smollea@gmail.com>
2019-05-30 11:48:31 +0200

    A08_GraphZusammenhängend implementiert

--- src/A08_GraphZusammenhängend/ConnectedComponents.java
+++ src/A08_GraphZusammenhängend/ConnectedComponents.java
@@ -1,6 +1,14 @@
 package A08_GraphZusammenhängend;
 
 import basisAlgorithmen.Graph;
+import basisAlgorithmen.ListGraph;
+import basisAlgorithmen.Vertex;
+import basisAlgorithmen.WeightedEdge;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
 
 public class ConnectedComponents {
 	
@@ -10,8 +18,30 @@ public class ConnectedComponents {
 	 * @return Anzahl der Komponenten
 	 */
 	public int getNumberOfComponents(Graph g) {
+		return getNumberOfComponents(g, 0);
+	}
 
-		return 0;
+	public int getNumberOfComponents(Graph g, int startVertex){
+		Stack<Integer> vertexStack = new Stack<>();
+		Set<Integer> vertexSet = new HashSet<>();
+		int numOfComponents = 1;
+		vertexStack.add(startVertex);
+		vertexSet.add(startVertex);
+		while (!vertexStack.empty()){
+			List<WeightedEdge> weightedEdges = g.getEdges(vertexStack.pop());
+			for (WeightedEdge weightedEdge : weightedEdges) {
+				if(vertexSet.add(weightedEdge.to_vertex)) {
+					vertexStack.add(weightedEdge.to_vertex);
+				}
+			}
+		}
+		for (int i = startVertex; i < g.numVertices(); i++) {
+			if(!vertexSet.contains(i)){
+				numOfComponents += getNumberOfComponents(g, i);
+				break;
+			}
+		}
+		return numOfComponents;
 	}
 
 }
